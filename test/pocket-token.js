a3f85e0cb565db9502992dea19071e2c187e69e8
Michael O'Rourke <>
2018-01-30 17:54:00 -0500

    added first tests

--- test/pocket-token.js
+++ test/pocket-token.js
@@ -7,91 +7,46 @@ contract('PocketToken', function(accounts) {
 
   var sender = accounts[0];
   var relayer = accounts[1];
-  var attacker = accounts[accounts.length - 1];
   var token;
   var registry;
 
-  it("Should check that stake value is greater than 0", function() {
-    return PocketToken.deployed().then(function(instance) {
-      token = instance;
-      token.stake(1, {from:sender});
-      return token.stakerAmount.call(sender);
-    }).then(function(amount) {
-      assert.isAbove(amount.toNumber(), 0, "greater than zero");
+  describe("Deploy all the contracts", function() {
 
+      it("should initialize token contract", function() {
+        return PocketToken.new().then(function(instance) {
+          token = instance;
+        });
+      });
+
+    it("should initialize registry contract", function() {
+      return PocketRegistry.new().then(function(instance) {
+        registry = instance;
+      });
+    });
+
+
+    it("should initialize registry delegate contract", function() {
+      return PocketRegistryDelegate.new().then(function(instance) {
+        registryDelegate = instance;
+      });
+    });
+
+    it("should initialize node delegate contract", function() {
+      return PocketNodeDelegate.new().then(function(instance) {
+        nodeDelegate = instance;
+      });
     });
   });
 
-  it("should release stake value", function() {
-    return PocketToken.deployed().then(function(instance) {
-      // token = instance;
-      token.releaseStake(1, {from:sender});
-      return token.stakerAmount.call(sender);
-    }).then(function(amount) {
-      assert.equal(amount.toNumber(),0, "equal to zero");
+  describe("Set delegates", function() {
 
-    });
-  });
-
-  it("Should check that stake value is equal", function() {
-    return PocketToken.deployed().then(function(instance) {
-      token = instance;
-      token.stake(1, {from:sender});
-      return token.stakerAmount.call(sender);
-    }).then(function(amount) {
-      assert.equal(amount.toNumber(), 1, "equal to 1");
-
-    });
-  });
-
-
-});
-
-
-
-contract('PocketRegistry', function(accounts) {
-
-  var sender = accounts[0];
-  var relayer = accounts[1];
-  var attacker = accounts[accounts.length - 1];
-  var token;
-  var registry;
-  var registryDelegate;
-  var supply;
-  var node;
-  var nodeDelegate;
-
-  before(function() {
-     return PocketToken.deployed().then(function(instance){
-        token = instance;
-     });
- });
-
- before(function() {
-    return PocketRegistryDelegate.deployed().then(function(instance){
-       registryDelegate = instance;
-    });
-});
-
-before(function() {
-   return PocketNodeDelegate.deployed().then(function(instance){
-      nodeDelegate = instance;
-   });
-});
-
-  it("should burn 1 token", function() {
-    PocketRegistry.deployed().then(function(instance) {
-
-      supply = token.totalSupply.call().toNumber();
-      registry = instance;
+    it("should set registry delegate", function() {
       registry.changeDelegate(registryDelegate.address);
-      registry.setNodeDelegateAddress(nodeDelegate.address);
-
-      registry.registerBurn(token.address, "test", {from:sender});
-      return token.totalSupply.call().toNumber();
-    }).then(function(amount) {
-      assert.equal(amount, supply, "supply is 1 less");
-
+      return registry.delegate.call().then(function(address) {
+      console.log(registryDelegate.address);
+      console.log(instance);
+        assert.equal(address.delegate, registryDelegate.address, "addresses should be equal");
+      });
     });
   });
 });
