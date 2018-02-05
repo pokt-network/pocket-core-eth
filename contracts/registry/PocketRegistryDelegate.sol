ec8baf9535067b3d850fd43f91307c9370e52116
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-02-05 17:26:51 -0400

    Renamed createNodeContract -> createNodeOracle Added require checks to the same method

--- contracts/registry/PocketRegistryDelegate.sol
+++ contracts/registry/PocketRegistryDelegate.sol
@@ -39,8 +39,13 @@ contract PocketRegistryDelegate is BaseRegistry {
 
   }
 
-  function createNodeContract(string[] _supportedTokens, string _url, uint8 _port, bool _isRelayer, bool _isOracle) {
-
+  function createNodeOracle(string[] _supportedTokens, string _url, uint8 _port, bool _isRelayer, bool _isOracle) {
+    require(_supportedTokens.count > 0)
+    require(_url.length > 0)
+    require(_port > 0)
+    require(_isRelayer)
+    require(_isOracle)
+    
     tokenAddress.call(bytes4(sha3("burn(uint256,address)")),1,msg.sender);
 
     PocketNode newNode = new PocketNode();
