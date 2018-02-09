508806740ef61a0ec3082573e51b7abf9732b3a2
Michael O'Rourke <>
2018-02-09 09:01:11 -0500

    testing

--- contracts/registry/PocketRegistryDelegate.sol
+++ contracts/registry/PocketRegistryDelegate.sol
@@ -13,18 +13,18 @@ contract PocketRegistryDelegate is NodeCrud, PocketRegistryState {
 
   // This is the function to create a new Node.
   function createNodeContract(bytes8[] _supportedTokens, bytes32 _url, bytes32 _path, uint8 _port, bool _isRelayer, bool _isOracle) public {
-    require(_supportedTokens.length > 0);
+    //require(_supportedTokens.length > 0);
     require(_url.length > 0);
-    require(_port > 0);
+    //require(_port > 0);
     require(_isRelayer);
     require(_isOracle);
 
     require(tokenAddress.call(bytes4(keccak256("burn(uint256,address)")),1,msg.sender));
 
-    PocketNodeInterface newNode = new PocketNodeInterface(msg.sender, nodeDelegateAddress, tokenAddress, _isRelayer, _isOracle);
-    userNode[msg.sender] = newNode;
+    //PocketNodeInterface newNode = new PocketNodeInterface(msg.sender, nodeDelegateAddress, tokenAddress, _isRelayer, _isOracle);
+    //userNode[msg.sender] = newNode;
 
-    registerNode(newNode, _supportedTokens, _url, _path, _port, _isRelayer, _isOracle);
+    //registerNode(newNode, _supportedTokens, _url, _path, _port, _isRelayer, _isOracle);
 
   }
 
@@ -52,12 +52,12 @@ contract PocketRegistryDelegate is NodeCrud, PocketRegistryState {
     selfdestruct(owner);
   }
 
-  function setTokenAddress(address _tokenAddress) public {
+  /* function setTokenAddress(address _tokenAddress) public {
     tokenAddress = _tokenAddress;
   }
 
   function setNodeDelegateAddress(address _nodeDelegateAddress) public {
     nodeDelegateAddress = _nodeDelegateAddress;
-  }
+  } */
 
 }
