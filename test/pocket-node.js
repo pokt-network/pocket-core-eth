f9f3ebf1825bbebc208537927c728154f0fd3f89
Luis de Leon <luyzdeleon@gmail.com>
2018-02-12 15:17:16 -0400

    Added test cases

---
+++ test/pocket-node.js
@@ -0,0 +1,33 @@
+var PocketNodeContract = artifacts.require('./PocketNode.sol'),
+    PocketNodeDelegateContract = artifacts.require('./PocketNodeDelegate.sol'),
+    chai = require('chai'),
+    expect = chai.expect,
+    should = chai.should();
+
+contract('PocketNode', (accounts) => {
+
+  it('should create a new instance', async() => {
+
+  });
+
+  it('should create a new relay', async() => {
+
+  });
+
+  it('should submit a vote to an existing relay', async() => {
+
+  });
+
+  it('should increase the count of approved and concluded relays done by this node', async() => {
+    
+  });
+
+  it('should withdraw funds to owner', async() => {
+
+  });
+
+  it('should transfer all Pocket funds to owner on kill', async() => {
+
+  });
+
+});
