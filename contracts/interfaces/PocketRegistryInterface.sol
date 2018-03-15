aa4af26bf2c3ae5fd5b625505fd9436ed65c28d1
Luis de Leon <luyzdeleon@gmail.com>
2018-03-14 21:52:42 -0400

    Removed old code

--- contracts/interfaces/PocketRegistryInterface.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4;
-
-contract PocketRegistryInterface {
-  function getRelayOracles() public returns(address[5] oracles) {}
-  function getLiveNodesCount() public returns(uint nodesCount) {}
-  function getNodeRecordAtIndex(uint _index) public returns(address key) {}
-}
