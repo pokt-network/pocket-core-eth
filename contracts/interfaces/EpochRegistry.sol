a91f4f8396fbcab9c8fd08715bc9a3f91dcef6f8
Luis de Leon <luyzdeleon@gmail.com>
2018-03-27 02:25:29 -0400

    Added interfaces

--- contracts/interfaces/EpochRegistry.sol
+++ contracts/interfaces/EpochRegistry.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.22;
+pragma solidity ^0.4.18;
 
 import "../Models.sol";
 
@@ -11,23 +11,20 @@ contract EpochRegistry {
 
     // Functions
     /*
-     * @dev Constructor function for this contract, it must initialize the first Epoch.
-     */
-    function EpochRegistry() {}
-    /*
      * @dev Adds a Relay to the current epoch indicated in the state of this contract.
      * @param bytes32 _txHash - The tx hash of this relay
      * @param address _sender - Sender of the relay
      * @param bytes32 _nodeNonce - The nonce pertaining to the Node doing this relay
      */
-    function addRelayToCurrentEpoch(bytes32 _txHash, address _sender, bytes32 _nodeNonce) public {}
+    function addRelayToCurrentEpoch(bytes8 _token, bytes32 _txHash, address _sender, bytes32 _nodeNonce) public returns(bytes32) {}
     /*
      * @dev Adds a verification of whether or not a given Relay was succesfully executed or not.
      *      The condition to be able to verify a Relay, is being a Node that executed another Relay
      *      in the same Epoch.
      * @param uint256 _epoch - The epoch where the relay happened.
      * @param bytes32 _relayTx - The relay that was verified
+     * @param bytes32 _nodeNonce - The nonce of the node sending the verification result
      * @param bool _verificationResult - The result of this verification
      */
-    function verifyRelayInEpoch(uint256 _epoch, bytes32 _relayTx, bool _verificationResult) public {}
+    function verifyRelayInEpoch(uint256 _epoch, bytes32 _relayNonce, bytes32 _nodeNonce, bool _verificationResult) public {}
 }
--- contracts/interfaces/NodeRegistry.sol
+++ contracts/interfaces/NodeRegistry.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.22;
+pragma solidity ^0.4.18;
 
 import "../Models.sol";
 
@@ -11,18 +11,24 @@ contract NodeRegistry {
     /*
      * @dev allows to register a new Node, returning a bytes32 nonce for the Node, if it doesn't exist already, in which case it will error out.
      * @param bytes8[] _networks - List of networks this Node supports (BTC, ETH, etc.).
-     * @param bytes32 _endpoint - The endpoint information in multi addr format.
+     * @param string _endpoint - The endpoint information in multi addr format.
      */
-    function register(bytes8[] _networks, bytes32 _endpoint) public returns(bytes32) {}
+    function register(bytes8[] _networks, string _endpoint) public returns(bytes32) {}
     /*
      * @dev Verify existence of a given Node nonce
      * @param bytes32 _nodeNonce - Nonce to verify against.
      */
-    function isNode(bytes32 _nodeNonce) public returns(bool) {}
+    function isNode(bytes32 _nodeNonce) public view returns(bool) {}
     /*
      * @dev Verify ownership of a given Node.
      * @param address _possibleOwner - The possible owner of the given node we want to verify
      * @param bytes32 _nodeNonce - The nonce of the Node we want to verify
      */
-    function isOwner(address _possibleOwner, bytes32 _nodeNonce) public returns(bool) {}
+    function isOwner(address _possibleOwner, bytes32 _nodeNonce) public view returns(bool) {}
+
+    /*
+     * @dev Returns the owner of the node
+     * @param bytes32 _nodeNonce
+     */
+    function getNodeOwner(bytes32 _nodeNonce) public view returns(address) {}
 }
